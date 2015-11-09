7edb79f7d259635fa2e93086c31b88ff085e2198
piotroramus <piotroramus@mail.com>
2015-11-09 10:05:11 +0100

    Calculated single events counters

--- run_single.sh
+++ run_single.sh
@@ -2,9 +2,9 @@
 
 for algorithm in 1 2 3 4 5 6 7 8 9 51 52
 do
-    for event in 1 2 3 4
+    for event in 0 1 2 3
     do
         gcc papi_metrics_single.c mm.c -o papi_metrics_single -lpapi -static -DALGORITHM=$algorithm -DEVENT=$event
-       ./papi_metrics
+       ./papi_metrics_single
     done
 done
