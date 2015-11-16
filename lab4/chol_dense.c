3ff76322623342c9ca8d33f15f52188ab429c840
piotroramus <piotroramus@mail.com>
2015-11-16 11:25:30 +0100

    Implemented loading matrix from file

--- lab4/chol_dense.c
+++ lab4/chol_dense.c
@@ -6,6 +6,8 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 #include "chol_dense.h"
 #include "clock.h"
@@ -14,8 +16,45 @@
 #define IDX(i, j, n) (((j)+ (i)*(n)))
 
 
-int load_matrix(char* filename){
+double* load_matrix(char* filename, int n){
 
+    char *line = NULL;
+    char *token = NULL;
+    size_t len = 0;
+    ssize_t read;
+    FILE *fp;
+    double *A;
+    char newline[2] = "\n";
+    int i = 0;
+    int j = 0;
+
+
+    fp = fopen(filename, "r");
+    if (fp == NULL){
+        printf("Cannot open input file\n");
+        exit(1);
+    }
+
+
+    A = calloc(n*n, sizeof(double));
+    assert(A != NULL);
+
+    while ((read = getline(&line, &len, fp)) != -1)
+        while ((token = strsep(&line, ",")) != NULL)
+            if (strcmp(token, newline) != 0){
+                A[IDX(i, j, n)] = atof(token);
+                j++;
+                if (j == n){
+                    j = 0;
+                    i++;
+                }
+            }
+
+    fclose(fp);
+    if (line)
+        free(line);
+
+    return A;
 }
 
 
@@ -78,34 +117,37 @@ speed_chol(double *A, unsigned int n)
 int
 main()
 {
+
     double *A;
-    int i, j, n, ret;
+    int i, j, n, ret, result;
+    char matrix_file[30];
+    double start_time, end_time;
 
-    n = 3;
-    A = calloc(n*n, sizeof(double));
-    assert(A != NULL);
+    n = 10;
+    sprintf(matrix_file, "input/matrix_%dx%d.txt", n, n);
 
-    A[IDX(0, 0, n)] = 4.0;   A[IDX(0, 1, n)] = 12.0;  A[IDX(0, 2, n)] = -16.0;
-    A[IDX(1, 0, n)] = 12.0;  A[IDX(1, 1, n)] = 37.0;  A[IDX(1, 2, n)] = -43.0;
-    A[IDX(2, 0, n)] = -16.0; A[IDX(2, 1, n)] = -43.0; A[IDX(2, 2, n)] = 98.0;
+    A = load_matrix(matrix_file, n);
 
-    double start_time = dclock();
-    int result = speed_chol(A, 3);
-//    int result = chol(A, 3);
-    double end_time = dclock();
+//    A[IDX(0, 0, n)] = 4.0;   A[IDX(0, 1, n)] = 12.0;  A[IDX(0, 2, n)] = -16.0;
+//    A[IDX(1, 0, n)] = 12.0;  A[IDX(1, 1, n)] = 37.0;  A[IDX(1, 2, n)] = -43.0;
+//    A[IDX(2, 0, n)] = -16.0; A[IDX(2, 1, n)] = -43.0; A[IDX(2, 2, n)] = 98.0;
+
+    start_time = dclock();
+    result = speed_chol(A, n);
+//    result = chol(A, n);
+    end_time = dclock();
 
     if (result != 0) {
         fprintf(stderr, "Error: matrix is either not symmetric or not positive definite.\n");
     } else {
         fprintf(stdout, "Execution time: %le\n", end_time - start_time);
-        fprintf(stdout, "Tri(L) = \n");
-        for (i = 0; i < 3; i++) {
-            for (j = 0; j <= i; j++)
-                printf("%2.8lf\t", A[IDX(i, j, n)]);
-            printf("\n");
-        }
+//        fprintf(stdout, "Tri(L) = \n");
+//        for (i = 0; i < n; i++) {
+//            for (j = 0; j <= i; j++)
+//                printf("%2.8lf\t", A[IDX(i, j, n)]);
+//            printf("\n");
+//        }
     }
-
 
     free(A);
     return 0;
--- lab4/run.sh
+++ lab4/run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+rm -f chol_dense
+
 ./compile.sh
 ./chol_dense
