3d8831a3e48275893037b91d1f2299a4f2b5f4ec
piotroramus <piotroramus@mail.com>
2015-11-16 17:40:44 +0100

    Added template for SIMD algorithm

--- lab5/chol_dense.c
+++ lab5/chol_dense.c
@@ -19,13 +19,18 @@
 
 int chol(double *A, unsigned int n);
 int speed_chol(double *A, unsigned int n);
+int simd_chol(double *A, unsigned int n);
 int assert_matrix_equality(double *A, double *B, int n);
 double* load_matrix(char* filename, int n);
 
 
 int main(){
 
-    double *A, *B;
+    //processor specs:
+    // http://www.cpu-world.com/CPUs/Core_i7/Intel-Core%20i7-3630QM%20Mobile%20processor.html
+
+
+    double *A, *B, *C;
     int i, j, n, ret, result;
     char matrix_file[30];
     double start_time, end_time, time;
@@ -37,11 +42,12 @@ int main(){
 
     A = load_matrix(matrix_file, n);
     B = load_matrix(matrix_file, n);
+    C = load_matrix(matrix_file, n);
 
     fprintf(stdout, "\n====================================================\n");
     fprintf(stdout, "\nMatrix size: %d\n\n", n);
 
-    fprintf(stdout, "Standard algorithm:\n");
+    fprintf(stdout, "Standard algorithm\n");
     start_time = dclock();
     result = chol(A, n);
     end_time = dclock();
@@ -63,7 +69,7 @@ int main(){
     }
 
 
-    fprintf(stdout, "\nOptimized algorithm:\n");
+    fprintf(stdout, "\nOptimized algorithm\n");
     start_time = dclock();
     result = speed_chol(B, n);
     end_time = dclock();
@@ -82,18 +88,41 @@ int main(){
         measure(speed_chol, B, n, event_type);
     }
 
+
+    fprintf(stdout, "\nSIMD algorithm\n");
+    start_time = dclock();
+    result = simd_chol(C, n);
+    end_time = dclock();
+
+    if (result != 0) {
+        fprintf(stderr, "Error: matrix is either not symmetric or not positive definite.\n");
+        exit(2);
+    } else {
+        time = end_time - start_time;
+        fprintf(stdout, "Execution time:\t\t\t\t %le\n", time);
+        fprintf(stdout, "MFLOPS:\t\t\t\t\t %f\n", gflops_prefix / time);
+    }
+
+    for (event_type = 0; event_type < 4; event_type++){
+        C = load_matrix(matrix_file, n);
+        measure(simd_chol, C, n, event_type);
+    }
+
+
     fprintf(stdout, "\n");
     fprintf(stdout, "====================================================\n");
 
-    if (assert_matrix_equality(A, B, n)){
+    if (assert_matrix_equality(A, C, n)){
         printf("Algorithms differ in results!\n");
         free(A);
         free(B);
+        free(C);
         exit(1);
     }
 
     free(A);
     free(B);
+    free(C);
     return 0;
 
 }
@@ -154,6 +183,70 @@ int speed_chol(double *A, unsigned int n){
                     }
             }
             // i <= 8
+            else for (k = 0; k < j; ++k)
+                    Aij -= A[IDX(i, k, local_size)] * A[IDX(j, k, local_size)];
+            A[IDX(i, j, local_size)] = Aij;
+        }
+
+        if (A[IDX(j, j, local_size)] < 0.0) {
+            return (1);
+        }
+
+        A[IDX(j, j, local_size)] = sqrt(A[IDX(j, j, local_size)]);
+        register double Ajj = A[IDX(j, j, n)];
+        for (i = j + 1; i < local_size;){
+
+            if (i < local_size - 8){
+                A[IDX(i, j, local_size)] /= Ajj;
+                A[IDX(i + 1, j, local_size)] /= Ajj;
+                A[IDX(i + 2, j, local_size)] /= Ajj;
+                A[IDX(i + 3, j, local_size)] /= Ajj;
+                A[IDX(i + 4, j, local_size)] /= Ajj;
+                A[IDX(i + 5, j, local_size)] /= Ajj;
+                A[IDX(i + 6, j, local_size)] /= Ajj;
+                A[IDX(i + 7, j, local_size)] /= Ajj;
+                i += 8;
+            } else {
+                A[IDX(i, j, local_size)] /= Ajj;
+                i++;
+            }
+        }
+    }
+
+    return (0);
+}
+
+
+int simd_chol(double *A, unsigned int n){
+
+    register unsigned int i;
+    register unsigned int j;
+    register unsigned int k;
+    register unsigned int local_size = n;
+
+    for (j = 0; j < local_size; j++) {
+        for (i = j; i < local_size; i++) {
+            register double Aij = A[IDX(i, j, local_size)];
+            if (j > 8)
+                for (k = 0; k < j;) {
+                    if (k < j - 8){
+
+
+                        Aij -= A[IDX(i, k, local_size)] * A[IDX(j, k, local_size)];
+                        Aij -= A[IDX(i, k + 1, local_size)] * A[IDX(j, k + 1, local_size)];
+                        Aij -= A[IDX(i, k + 2, local_size)] * A[IDX(j, k + 2, local_size)];
+                        Aij -= A[IDX(i, k + 3, local_size)] * A[IDX(j, k + 3, local_size)];
+                        Aij -= A[IDX(i, k + 4, local_size)] * A[IDX(j, k + 4, local_size)];
+                        Aij -= A[IDX(i, k + 5, local_size)] * A[IDX(j, k + 5, local_size)];
+                        Aij -= A[IDX(i, k + 6, local_size)] * A[IDX(j, k + 6, local_size)];
+                        Aij -= A[IDX(i, k + 7, local_size)] * A[IDX(j, k + 7, local_size)];
+                        k = k + 8;
+                    } else {
+                        Aij -= A[IDX(i, k, local_size)] * A[IDX(j, k, local_size)];
+                        k++;
+                    }
+                }
+                // i <= 8
             else for (k = 0; k < j; ++k)
                     Aij -= A[IDX(i, k, local_size)] * A[IDX(j, k, local_size)];
             A[IDX(i, j, local_size)] = Aij;
