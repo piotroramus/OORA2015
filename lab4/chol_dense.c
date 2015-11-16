3105b95f1512f253f94de2f4fe6f8080e32fea51
piotroramus <piotroramus@mail.com>
2015-11-16 11:42:28 +0100

    Reformatted code and checked if matrices are equal

--- lab4/chol_dense.c
+++ lab4/chol_dense.c
@@ -8,12 +8,127 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 #include "chol_dense.h"
 #include "clock.h"
 
 
 #define IDX(i, j, n) (((j)+ (i)*(n)))
+
+
+int chol(double *A, unsigned int n);
+int speed_chol(double *A, unsigned int n);
+int assert_matrix_equality(double *A, double *B, int n);
+double* load_matrix(char* filename, int n);
+
+
+int main(){
+
+    double *A, *B;
+    int i, j, n, ret, result1, result2;
+    char matrix_file[30];
+    double start_time1, end_time1, start_time2, end_time2;
+
+    n = 10;
+    sprintf(matrix_file, "input/matrix_%dx%d.txt", n, n);
+
+    A = load_matrix(matrix_file, n);
+    B = load_matrix(matrix_file, n);
+
+    start_time1 = dclock();
+    result1 = chol(A, n);
+    end_time1 = dclock();
+
+    start_time2 = dclock();
+    result2 = speed_chol(B, n);
+    end_time2 = dclock();
+
+    if (assert_matrix_equality(A, B, n)){
+        printf("Algorithms differ in results!\n");
+        free(A);
+        free(B);
+        exit(1);
+    }
+
+    if (result1 != 0) {
+        fprintf(stderr, "Error: matrix is either not symmetric or not positive definite.\n");
+    } else
+        fprintf(stdout, "\nStandard algorithm execution time:\t %le\n", end_time1 - start_time1);
+
+    if (result2 != 0) {
+        fprintf(stderr, "Error: matrix is either not symmetric or not positive definite.\n");
+    } else
+        fprintf(stdout, "Optimized algorithm execution time:\t %le\n\n", end_time2 - start_time2);
+
+
+    free(A);
+    free(B);
+    return 0;
+
+}
+
+
+int chol(double *A, unsigned int n){
+
+    unsigned int i;
+    unsigned int j;
+    unsigned int k;
+
+    for (j = 0; j < n; j++) {
+        for (i = j; i < n; i++) {
+            for (k = 0; k < j; ++k) {
+                A[IDX(i, j, n)] -= A[IDX(i, k, n)] *
+                                   A[IDX(j, k, n)];
+            }
+        }
+
+        if (A[IDX(j, j, n)] < 0.0) {
+            return (1);
+        }
+
+        A[IDX(j, j, n)] = sqrt(A[IDX(j, j, n)]);
+        for (i = j + 1; i < n; i++)
+            A[IDX(i, j, n)] /= A[IDX(j, j, n)];
+    }
+
+    return (0);
+}
+
+
+int speed_chol(double *A, unsigned int n){
+
+    register unsigned int i;
+    register unsigned int j;
+    register unsigned int k;
+
+    for (j = 0; j < n; j++) {
+        for (i = j; i < n; i++) {
+            for (k = 0; k < j; ++k) {
+                A[IDX(i, j, n)] -= A[IDX(i, k, n)] *
+                                   A[IDX(j, k, n)];
+            }
+        }
+
+        if (A[IDX(j, j, n)] < 0.0) {
+            return (1);
+        }
+
+        A[IDX(j, j, n)] = sqrt(A[IDX(j, j, n)]);
+        for (i = j + 1; i < n; i++)
+            A[IDX(i, j, n)] /= A[IDX(j, j, n)];
+    }
+
+    return (0);
+}
+
+
+int assert_matrix_equality(double *A, double *B, int n){
+    int i, j;
+    for (i = 0; i < n; i++)
+        for (j = 0; j <= i; j++)
+            if (A[IDX(i, j, n)] != B[IDX(i, j, n)])
+                return 1;
+    return 0;
+}
 
 
 double* load_matrix(char* filename, int n){
@@ -56,100 +171,3 @@ double* load_matrix(char* filename, int n){
 
     return A;
 }
-
-
-int
-chol(double *A, unsigned int n)
-{
-    unsigned int i;
-    unsigned int j;
-    unsigned int k;
-
-    for (j = 0; j < n; j++) {
-        for (i = j; i < n; i++) {
-            for (k = 0; k < j; ++k) {
-                A[IDX(i, j, n)] -= A[IDX(i, k, n)] *
-                                   A[IDX(j, k, n)];
-            }
-        }
-
-        if (A[IDX(j, j, n)] < 0.0) {
-            return (1);
-        }
-
-        A[IDX(j, j, n)] = sqrt(A[IDX(j, j, n)]);
-        for (i = j + 1; i < n; i++)
-            A[IDX(i, j, n)] /= A[IDX(j, j, n)];
-    }
-
-    return (0);
-}
-
-int
-speed_chol(double *A, unsigned int n)
-{
-
-    register unsigned int i;
-    register unsigned int j;
-    register unsigned int k;
-
-    for (j = 0; j < n; j++) {
-        for (i = j; i < n; i++) {
-            for (k = 0; k < j; ++k) {
-                A[IDX(i, j, n)] -= A[IDX(i, k, n)] *
-                                   A[IDX(j, k, n)];
-            }
-        }
-
-        if (A[IDX(j, j, n)] < 0.0) {
-            return (1);
-        }
-
-        A[IDX(j, j, n)] = sqrt(A[IDX(j, j, n)]);
-        for (i = j + 1; i < n; i++)
-            A[IDX(i, j, n)] /= A[IDX(j, j, n)];
-    }
-
-    return (0);
-}
-
-
-int
-main()
-{
-
-    double *A;
-    int i, j, n, ret, result;
-    char matrix_file[30];
-    double start_time, end_time;
-
-    n = 10;
-    sprintf(matrix_file, "input/matrix_%dx%d.txt", n, n);
-
-    A = load_matrix(matrix_file, n);
-
-//    A[IDX(0, 0, n)] = 4.0;   A[IDX(0, 1, n)] = 12.0;  A[IDX(0, 2, n)] = -16.0;
-//    A[IDX(1, 0, n)] = 12.0;  A[IDX(1, 1, n)] = 37.0;  A[IDX(1, 2, n)] = -43.0;
-//    A[IDX(2, 0, n)] = -16.0; A[IDX(2, 1, n)] = -43.0; A[IDX(2, 2, n)] = 98.0;
-
-    start_time = dclock();
-    result = speed_chol(A, n);
-//    result = chol(A, n);
-    end_time = dclock();
-
-    if (result != 0) {
-        fprintf(stderr, "Error: matrix is either not symmetric or not positive definite.\n");
-    } else {
-        fprintf(stdout, "Execution time: %le\n", end_time - start_time);
-//        fprintf(stdout, "Tri(L) = \n");
-//        for (i = 0; i < n; i++) {
-//            for (j = 0; j <= i; j++)
-//                printf("%2.8lf\t", A[IDX(i, j, n)]);
-//            printf("\n");
-//        }
-    }
-
-    free(A);
-    return 0;
-}
-
